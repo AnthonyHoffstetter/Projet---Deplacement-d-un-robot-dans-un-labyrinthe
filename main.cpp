826eee9b1a95cd2241f03521308e860ca77cf362
AnthonyHoffstetter <anthony.hoffstetter@outlook.fr>
2024-11-22 00:04:42 +0100

    Revert "Creation de la classe position"
    This reverts commit d24376b8b6bdc30c0508304a74f2450d343ebe61.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
-#include "position.h"
 
 using namespace std;
 
-void testPosition()
-{
-    position p1(2,5);
-    position p2(3,7);
-    position p3(2,5);
-    cout<<"p1.x = "<<p1.getX()<<" et p1.y = "<<p1.getY()<<endl;
-    if(p1==p2)
-        cout<<"p1 et p2 sont egaux"<<endl;
-    if(p1==p3)
-        cout<<"p1 et p3 sont egaux"<<endl;
-    if(p1!=p2)
-        cout<<"p1 et p2 sont differents"<<endl;
-}
-
 int main()
 {
-    //testPosition();
+
 }
