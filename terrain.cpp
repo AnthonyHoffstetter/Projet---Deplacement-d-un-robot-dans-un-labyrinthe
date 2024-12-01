188a50169bbac8d3d371abd926538d7176fe8c8e
didou6661 <makhlouf.sanad@gmail.com>
2024-12-01 21:08:54 +0100

    le  reste  de la classe terrain

--- terrain.cpp
+++ terrain.cpp
@@ -36,3 +36,40 @@ bool Terrain::chargerDepuisFichier(const string& nomFichier) {
     fichier.close();
     return true;
 }
+
+
+void Terrain::afficher() const {
+    for (const auto& ligne : d_terrain) {
+        for (char c : ligne) {
+            cout << c;
+        }
+        cout << endl;
+    }
+}
+
+int Terrain::getLargeur() const {
+    return d_largeur;
+}
+
+int Terrain::getLongueur() const {
+    return d_longueur;
+}
+
+position Terrain::getCaseDepart() const {
+    return d_depart;
+}
+
+position Terrain::getCaseArrivee() const {
+    return d_arrivee;
+}
+
+bool Terrain::estLibre(const position& sp) const {
+    int x = sp.getX();
+    int y = sp.getY();
+
+    if (x < 0 || x >= d_largeur || y < 0 || y >= d_longueur) {
+        return false; 
+    }
+
+    return d_terrain[y][x] == '.' || d_terrain[y][x] == 'A';
+}
