ea6edfa744cb6c6e4dd5026580ceaff609d68320
AnthonyHoffstetter <anthony.hoffstetter@outlook.fr>
2024-12-02 00:40:52 +0100

    Déclaration de la méthode executer pour l'algorithme main droite

--- algorithmemaindroite.cpp
+++ algorithmemaindroite.cpp
@@ -1,3 +1,26 @@
 #include "algorithmemaindroite.h"
 
-
+void algorithmeMainDroite::executer(robot& r,const terrain& ter)
+{
+    // Tant que le robot n'est pas à la sortie
+    while (r.getPositionActuelle() != ter.getCaseArrivee())
+    {
+        // Si le robot peut tourner à droite
+        if (!r.detecterObstacleDroite(ter))
+        {
+            r.tournerDroite();
+            r.avancer();
+        }
+        // Sinon, s'il peut avancer tout droit
+        else if (!r.detecterObstacleDevant(ter))
+        {
+            r.avancer();
+        }
+        // Sinon, tourner à gauche
+        else
+        {
+            r.tournerGauche();
+        }
+        cout<<"position du robot : "<<r.getPositionActuelle().getX()<<','<<r.getPositionActuelle().getY()<<endl;
+    }
+}
