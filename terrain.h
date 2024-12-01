4b1e436e1fcd9a248058e2c095cd4e48dcd1bdfb
didou6661 <makhlouf.sanad@gmail.com>
2024-12-01 11:45:14 +0100

    ajoute la declaration de terrain

---
+++ terrain.h
@@ -0,0 +1,35 @@
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include "position.h" 
+
+class Terrain {
+public:
+
+    Terrain();
+
+    bool chargerDepuisFichier(const std::string& nomFichier);
+
+    void afficher() const;
+
+    int getLargeur() const;
+    int getLongueur() const;
+
+    position getCaseDepart() const;
+    position getCaseArrivee() const;
+
+    bool estLibre(const position& sp) const;
+
+private:
+    std::vector<std::vector<char>> d_terrain; 
+    int d_largeur;  
+    int d_longueur; 
+    position d_depart;   
+    position d_arrivee;  
+};
+
+#endif 
+
