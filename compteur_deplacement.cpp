430143928c2f48cd36613ba1f531fff18d95ffdd
makhlouf sanad <makhlouf.sanad@gmail.com>
2024-12-16 10:59:28 +0100

    Update compteur_deplacement.cpp le methode  compteur_deplacement()

--- compteur_deplacement.cpp
+++ compteur_deplacement.cpp
@@ -2,7 +2,7 @@
 
 compteur_deplacement::compteur_deplacement() : d_nombreDeplacements(0) {}
 
-void compteur_deplacement::compterDeplacement(const robot& r) {
+void compteur_deplacement::compterDeplacement() {
     // On suppose que chaque appel à cette méthode signifie que le robot se déplace
     d_nombreDeplacements++;
 }
