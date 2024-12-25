8954ad1500e19a932897b69967f2b66b03531e0d
AnthonyHoffstetter <anthony.hoffstetter@outlook.fr>
2024-12-26 00:14:28 +0100

    Rectification de la fonction update de l'afficheurRobot
    Rectification pour que l'afficheur commence au bon endroit

--- afficheurrobot.cpp
+++ afficheurrobot.cpp
@@ -2,9 +2,9 @@
 
 void afficheurRobot::update(const robot &r)
 {
-    goto_xy(r.getAnciennePosition().getX(),r.getAnciennePosition().getY());
+    goto_xy(r.getAnciennePosition().getX(),r.getAnciennePosition().getY()+2);
     std::cout<<".";
-    goto_xy(r.getPositionActuelle().getX(),r.getPositionActuelle().getY());
+    goto_xy(r.getPositionActuelle().getX(),r.getPositionActuelle().getY()+2);
     std::cout<<r.getDirection();
     Sleep(500);
 }
