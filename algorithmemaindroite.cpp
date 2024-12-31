4275b4202e6c9cae7b6fa53a17caedf4e9031c21
AnthonyHoffstetter <anthony.hoffstetter@outlook.fr>
2024-12-31 12:28:05 +0100

    Update algorithmemaindroite.cpp
    Suppression de commentaires inutiles

--- algorithmemaindroite.cpp
+++ algorithmemaindroite.cpp
@@ -2,21 +2,17 @@
 
 void algorithmeMainDroite::executer(robot& r,const terrain& ter)
 {
-    // Tant que le robot n'est pas à la sortie
     while (r.getPositionActuelle() != ter.getCaseArrivee())
     {
-        // Si le robot peut tourner à droite
         if (!r.detecterObstacleDroite(ter))
         {
             r.tournerDroite();
             r.avancer();
         }
-        // Sinon, s'il peut avancer tout droit
         else if (!r.detecterObstacleDevant(ter))
         {
             r.avancer();
         }
-        // Sinon, tourner à gauche
         else
         {
             r.tournerGauche();
