e57ff864f43e440cbc0507d2801defbf19f2b8fa
AnthonyHoffstetter <anthony.hoffstetter@outlook.fr>
2025-01-03 16:27:45 +0100

    Suppression des commentaires inutiles

--- algorithmepledge.cpp
+++ algorithmepledge.cpp
@@ -5,32 +5,28 @@ using namespace std;
 
 void algorithmePledge::executer(robot &r, const terrain &t)
 {
-    int compteurOrientation = 0; // Compteur pour suivre les rotations du robot
+    int compteurOrientation = 0;
 
-    // Tant que le robot n'est pas � la sortie
     while (r.getPositionActuelle() != t.getCaseArrivee())
     {
         if (compteurOrientation == 0)
         {
-            // Si le compteur est � z�ro, on avance directement
             if (!r.detecterObstacleDevant(t))
             {
                 r.avancer();
             }
             else
             {
-                // Sinon, on commence � contourner les obstacles
                 r.tournerGauche();
-                compteurOrientation++; // Ajouter +1 pour la rotation � gauche
+                compteurOrientation++;
             }
         }
         else
         {
-            // Suivre la m�thode de la main droite
             if (!r.detecterObstacleDroite(t))
             {
                 r.tournerDroite();
-                compteurOrientation--; // Soustraire -1 pour la rotation � droite
+                compteurOrientation--;
                 r.avancer();
             }
             else if (!r.detecterObstacleDevant(t))
@@ -40,10 +36,10 @@ void algorithmePledge::executer(robot &r, const terrain &t)
             else
             {
                 r.tournerGauche();
-                compteurOrientation++; // Ajouter +1 pour la rotation � gauche
+                compteurOrientation++;
             }
         }
     }
 
-  
+
 }
